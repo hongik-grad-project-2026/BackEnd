92e542da5accb02a2fc0f9d80c284c960acddf24
HeebinMAC <heebin_mac@HeebinMACui-MacBookAir.local>
2026-03-16 17:35:22 +0900

    [setting] init commit

---
+++ src/main/java/com/mulmi/backend/MulmiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mulmi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulmiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MulmiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mulmi/backend/MulmiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulmi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulmiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
