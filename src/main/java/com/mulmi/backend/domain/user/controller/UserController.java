39dd94eb632e5b62b142838377091c287b3c9583
heebindev <heebin1999@naver.com>
2026-06-24 12:12:16 +0900

    [feat] UserController 생성 및 회원가입 매핑

--- src/main/java/com/mulmi/backend/domain/user/controller/UserController.java
+++ src/main/java/com/mulmi/backend/domain/user/controller/UserController.java
@@ -3,18 +3,25 @@ package com.mulmi.backend.domain.user.controller;
 import com.mulmi.backend.domain.user.dto.request.SignupRequestDTO;
 import com.mulmi.backend.domain.user.dto.response.SignupResponseDTO;
 import com.mulmi.backend.global.apiPayload.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
 
+    private final UserService userService;
+
     //회원가입
+    @PostMapping
     public ApiResponse<SignupResponseDTO> signup(
-            @RequestBody SignupRequestDTO dto){
-        return null;
+            @RequestBody @Valid SignupRequestDTO dto){
+        return ApiResponse.onSuccess(UserSuccessCode.CREATED, userService.signup(dto));
     }
 
     //로그인
