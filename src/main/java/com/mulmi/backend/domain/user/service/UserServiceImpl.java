57f4d39663d23845a5e9b337447d94c182fd36e3
heebindev <heebin1999@naver.com>
2026-06-30 18:40:56 +0900

    [feat] 로그인 서비스 로직 구현

--- src/main/java/com/mulmi/backend/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/mulmi/backend/domain/user/service/UserServiceImpl.java
@@ -1,13 +1,16 @@
 package com.mulmi.backend.domain.user.service;
 
 import com.mulmi.backend.domain.user.converter.UserConverter;
+import com.mulmi.backend.domain.user.dto.request.LoginRequestDTO;
 import com.mulmi.backend.domain.user.dto.request.SignupRequestDTO;
+import com.mulmi.backend.domain.user.dto.response.LoginResponseDTO;
 import com.mulmi.backend.domain.user.dto.response.SignupResponseDTO;
 import com.mulmi.backend.domain.user.entity.User;
 import com.mulmi.backend.domain.user.exception.UserException;
 import com.mulmi.backend.domain.user.exception.code.UserErrorCode;
 import com.mulmi.backend.domain.user.repository.UserRepository;
 
+import com.mulmi.backend.global.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,7 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
 
     //회원가입
@@ -33,6 +37,32 @@ public class UserServiceImpl implements UserService {
         User savedUser = userRepository.save(user);
 
         return UserConverter.toSignupResponseDTO(savedUser);
+    }
+
+    //로그인
+    @Override
+    public LoginResponseDTO login(LoginRequestDTO dto) {
+        User user = userRepository.findByLoginId(dto.loginId())
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+
+        if (!passwordEncoder.matches(dto.password(), user.getPassword())){
+            throw new UserException(UserErrorCode.INVALID_PASSWORD);
+        }
+
+        String accessToken = jwtUtil.createAccessToken(
+                user.getId(),
+                user.getLoginId(),
+                user.getRole()
+        );
+
+        return new LoginResponseDTO(
+                user.getId(),
+                user.getLoginId(),
+                user.getName(),
+                user.getRole(),
+                user.getStatus(),
+                accessToken
+        );
     }
 
     //중복 검사 메서드
